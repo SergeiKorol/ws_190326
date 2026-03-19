c58394a7d02989443fbb90c8d5a59ed1e5782a4e
Ariko2607 <anime100398@gmail.com>
2026-03-19 21:33:39 +0300

    Добавлены коментарии

--- test/anastasia_test.py
+++ test/anastasia_test.py
@@ -2,14 +2,14 @@ import requests
 
 
 def create_delete_task():
-
+    # Создание задачи
     body = {"title": "temporary task", "completed": False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     assert response.status_code == 200
     task_id = response.json()["id"]
-
+    # удаление задачи
     response = requests.delete(f'https://todo-app-sky.herokuapp.com/{task_id}')
     assert response.status_code == 204
-
+    # Проверка, что задача удалена 
     response = requests.get(f'https://todo-app-sky.herokuapp.com/{task_id}')
     assert response.status_code == 404
