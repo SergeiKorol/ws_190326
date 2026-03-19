967afa32eaa7001dddb23727e029d1ae0bb6d7ad
Ariko2607 <anime100398@gmail.com>
2026-03-19 21:22:54 +0300

    Создание и удаление

--- test/anastasia_test.py
+++ test/anastasia_test.py
@@ -2,7 +2,7 @@ import requests
 
 
 def create_delete_task():
-  
+
     body = {"title": "temporary task", "completed": False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     assert response.status_code == 200
