bf221bacb4ffea7adbe50294894dc30c043f2535
aanitsirk <k.ovsyannikova2702@gmail.com>
2026-03-19 21:40:26 +0300

    Кристина исправила замечания

--- test/kristina_test.py
+++ test/kristina_test.py
@@ -4,6 +4,7 @@ url = "https://todo-app-sky.herokuapp.com/"
 
 
 def test_get_list_with_deleted_task():
+    # Тест на проверку статус-кода 404 при получении списка задач после создания и удаления созданной задачи.
     body = {"title": "Создать задачу", "completed": False}
     response = requests.post(url, json=body)
     response_body = response.json()
