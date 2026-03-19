1f9b18820d820e2758b65be93d9dc24bdd18ee9c
ZojaDD <zojadr.zd@gmail.com>
2026-03-19 21:40:10 +0300

    123

--- test/new_test.py
+++ test/new_test.py
@@ -1,6 +1,10 @@
 import requests
 
 def test_add():
+    """
+    попытаться создать задачу со статусом выполнено
+    сразу и убедить что так не получится.
+    """
     body = {"title": "generated", "completed": True}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     response_body = response.json()
